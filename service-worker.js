5c7f37de9c05264400eee0bd801dd5ea7f960256
Brar K. <shabadmail@gmail.com>
2025-03-16 14:59:51 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,7 @@
-const CACHE_NAME = "CSB-101"; // Change version to force update
-
-self.addEventListener("install", (event) => {
-    console.log("Service Worker installed");
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cache) => {
-                    return caches.delete(cache); // Clear old cache
-                })
-            );
-        })
-    );
-    self.skipWaiting(); // Activate new service worker immediately
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installed');
 });
 
-self.addEventListener("activate", (event) => {
-    console.log("Service Worker activated");
-    event.waitUntil(clients.claim()); // Apply updates to all open tabs
-});
-
-self.addEventListener("fetch", (event) => {
-    console.log("Fetching:", event.request.url);
-    event.respondWith(fetch(event.request)); // Always fetch fresh content
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
 });
