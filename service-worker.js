7e29f68649c19e37d770372c694b856e0804e5a4
Brar K. <shabadmail@gmail.com>
2025-03-21 19:03:08 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'my-app-cache-v34';
+const CACHE_NAME = 'my-app-cache-v35';
 const urlsToCache = [
   '/', '/index.html', '/com.js', '/ham.js', '/counter.js', 
   '/cheat.js', '/uti.js', '/icon-192.png', '/icon-256.png', 
